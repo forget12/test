9314a47efbe24237cb8bbdef055375a95a8cf163
l <fairytaler@vip.qq.com>
2022-01-04 14:31:32 +0800

    1

--- 标识符.py
+++ 标识符.py
@@ -22,3 +22,24 @@ print(bool({}))
 print(bool([]))
 print(bool(()))
 print(bool(None))
+a = 123
+
+print(bin(a))
+print(oct(a))
+print(hex(a))
+
+print(9.0 // 3.0)
+
+print(5.5 ** 2)
+
+
+# print('123' == 123)
+# print('ac' < 'ab')
+
+4 > 3 or print('哈哈哈')
+# 4 < 3 or print('嘿嘿嘿')
+
+print(not (5 > 2))
+
+4 > 3 and print('hello world')
+4 < 3 and print('你好世界')
